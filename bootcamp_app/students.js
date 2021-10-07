a26f88ab55be33b8c53fd97ab0933ebd1df87ac9
bilykb <brettbilyk@gmail.com>
2021-10-07 22:41:42 +0000

    Add postgres query for students

---
+++ bootcamp_app/students.js
@@ -0,0 +1,26 @@
+const { Pool } = require('pg');
+const { argv } = require('process');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+const cohort = argv[2]
+const limit = argv[3]
+
+pool.query(`
+SELECT students.id, students.name AS student, cohorts.name AS cohort
+FROM students
+JOIN cohorts ON students.cohort_id = cohorts.id
+WHERE cohorts.name LIKE '${cohort}%'
+LIMIT ${limit};
+`)
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.student} has an id of ${user.id} and was in the ${user.cohort} cohort`)
+  })
+})
+.catch(err => console.error('query error', err.stack))
